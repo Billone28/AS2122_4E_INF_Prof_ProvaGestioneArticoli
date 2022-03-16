11f3e5332bedcfc9460a55424e928e5aa60a1632
Danilo Sanchi <danilo.sanchi@studenti.ittsrimini.edu.it>
2022-03-16 09:42:21 +0100

    aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -33,7 +33,7 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
                 // aggiungi l'articolo all'elenco di articoli
                 // TODO: (1) aggiungere l'articolo creato dai dati di frmArticoli nella lista articoli
                 // ...
-                
+                Articolo a = new Articolo();
                 lblArticoliInseriti.Text = $"Articoli ({articoli.Count})";
             }
         }
