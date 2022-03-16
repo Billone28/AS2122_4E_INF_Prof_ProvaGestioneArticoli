2f24dbcc1c0722d305f8085d50f3d1f8980082cc
Danilo Sanchi <danilo.sanchi@studenti.ittsrimini.edu.it>
2022-03-16 09:53:49 +0100

    aggiungere property di sola lettura dei dati necessari all'esterno di Articolo

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/Articolo.cs
@@ -16,6 +16,10 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (9) aggiungere property di sola lettura dei dati necessari all'esterno di Articolo
         // ...
+        public string Descrizione { get; }
+        public string UnitaMisura { get; }
+        public string Prezzo { get; }
+
         public Articolo(int codice, string descrizione, string unitaMisura, double prezzo)
         {
             // TODO: (10) impostazione costruttore classe Articolo come utilizzato da frmMain
