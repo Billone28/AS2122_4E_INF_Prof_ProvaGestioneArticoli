f76873e483b2601e2cd21322a63906d76ff5e8b1
Danilo Sanchi <danilo.sanchi@studenti.ittsrimini.edu.it>
2022-03-16 10:08:41 +0100

    aggiungere visualizzazione articoli inseriti nella listbox

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmMain.cs
@@ -46,7 +46,8 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
             {
                 case "Visualizza articoli":
                     // TODO: (2) aggiungere visualizzazione articoli inseriti nella listbox
-                    // ... lstElenco.Items.Add(a.Visualizzati()); 
+                    // ... lstElenco.Items.Add(a.Visualizzati());
+                    lstElenco.ItemHeight.Add(articoli.Visualizzati());
                     break;
             }
         }
